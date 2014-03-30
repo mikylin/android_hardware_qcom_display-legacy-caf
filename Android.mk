32be6f6064392d88c0f65f9183c41f698ef9d974
aidejuewang <aidejuewang@gmail.com>
2014-03-30 21:44:15 +0800

    xiaomi audio HAL

---
+++ Android.mk
@@ -0,0 +1,13 @@
+#Enables the listed display HAL modules
+#libs to be built for QCOM targets only
+
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy-caf)
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+display-hals := libgralloc libgenlock libcopybit liblight
+display-hals += libhwcomposer liboverlay libqdutils
+endif
+
+display-hals += libtilerenderer
+
+include $(call all-named-subdir-makefiles,$(display-hals))
+endif
---
+++ common.mk
@@ -0,0 +1,34 @@
+#Common headers
+common_includes := hardware/qcom/display-legacy-caf/libgralloc
+common_includes += hardware/qcom/display-legacy-caf/libgenlock
+common_includes += hardware/qcom/display-legacy-caf/liboverlay
+common_includes += hardware/qcom/display-legacy-caf/libcopybit
+common_includes += hardware/qcom/display-legacy-caf/libqdutils
+
+common_header_export_path := qcom/display
+
+ifeq ($(TARGET_USES_POST_PROCESSING),true)
+    common_flags     += -DUSES_POST_PROCESSING
+    common_includes += $(TARGET_OUT_HEADERS)/pp/inc
+endif
+
+
+#Common libraries external to display HAL
+common_libs := liblog libutils libcutils libhardware
+
+#Common C flags
+common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers
+
+ifeq ($(ARCH_ARM_HAVE_NEON),true)
+    common_flags += -D__ARM_HAVE_NEON
+endif
+
+common_deps  :=
+kernel_includes :=
+#Kernel includes
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+    common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+    kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+endif
+
+
